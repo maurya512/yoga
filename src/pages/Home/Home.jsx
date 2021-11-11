e46b25bb0463f0241c1db06062e56f66b415d76d
maurya512 <patelmaurya0512@gmail.com>
2021-11-10 23:56:32 -0500

    Added images and styling for the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+// import images
+import yoga1 from "../../images/yogaimg1.svg";
 
 const Home = () => {
-    return (
-        <h1>
-           This is a home page 
-        </h1>
-    )
-}
+  return (
+    <>
+      <section id="header" className="d-flex align-items-center">
+        <div className="container-fluid nav_bg">
+          <div className="row">
+            <div className="col-10 mx-auto">
+              {/* separates the screen and their contents into 2 things */}
+              <div className="row">
+                {/* left side of the screen */}
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                  <h1>
+                    This year the focus should be on{" "}
+                    <strong className="brand-name">YOU!!</strong>
+                  </h1>
+                  {/* margin top and bottom 3 */}
+                  <h2 className="my-3">
+                    Your first step towards a healthy life style
+                  </h2>
+                  <div className="mt-3">
+                    <NavLink to="/about" className="btn-get-started">
+                      Get Started
+                    </NavLink>
+                  </div>
+                </div>
 
-export default Home
+                {/* header image */}
+                <div className="col-lg-6 order-1 order-lg-2 header-img">
+                  <img
+                    src={yoga1}
+                    className="img-fluid animated"
+                    alt="home screen img"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
