f458ee880a8f2057cb8cb9e8d7733984ae88f35a
maurya512 <patelmaurya0512@gmail.com>
2021-11-10 23:56:07 -0500

    added home page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,49 +13,50 @@ const Navbar = () => {
           {/* 10 out of 12 columns to ensure that the div is centered correctly  */}
           <div className="col-10 mx-auto">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <NavLink className="navbar-brand" to="/">
-                Yoga
-              </NavLink>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
+              <div className="container-fluid">
+                <NavLink className="navbar-brand" to="/">
+                  Yoga
+                </NavLink>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
 
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                {/* ! the newer version of bootstrap uses ms instead of ml */}
-                <ul className="navbar-nav ms-auto">
-                  <li className="nav-item">
+                <div
+                  className="collapse navbar-collapse"
+                  id="navbarSupportedContent"
+                >
+                  {/* ! the newer version of bootstrap uses ms instead of ml */}
+                  <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
                       {/* changed from anchor tag to navlink to make sure the page doesn't render while changing components */}
-                    <NavLink exact className="nav-link" to="/">
-                      Home <span className="sr-only"></span>
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink exact className="nav-link" to="/service">
-                      Services
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink exact className="nav-link" to="/about">
-                      About
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink exact className="nav-link" to="/contact">
-                      Contact
-                    </NavLink>
-                  </li>
-                  {/* <li className="nav-item dropdown">
+                      <NavLink exact className="nav-link" to="/">
+                        Home <span className="sr-only"></span>
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink exact className="nav-link" to="/service">
+                        Services
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink exact className="nav-link" to="/about">
+                        About
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink exact className="nav-link" to="/contact">
+                        Contact
+                      </NavLink>
+                    </li>
+                    {/* <li className="nav-item dropdown">
               <NavLink
                 className="nav-link dropdown-toggle"
                 to="#"
@@ -80,7 +81,8 @@ const Navbar = () => {
                 </NavLink>
               </div>
             </li> */}
-                </ul>
+                  </ul>
+                </div>
               </div>
             </nav>
           </div>
