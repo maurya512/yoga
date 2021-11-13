86ca0f5a8899257a1778f8b986e3f2b9cc1d8786
maurya512 <patelmaurya0512@gmail.com>
2021-11-12 23:38:48 -0500

    about now passes data to the common component which then renders all the data on the screen

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,42 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Common from "../../components/Common/Common";
 // import images
 import yoga2 from "../../images/yogaimg1.svg";
 
 const About = () => {
   return (
     <>
-      <section id="header" className="d-flex align-items-center">
-        <div className="container-fluid nav_bg">
-          <div className="row">
-            <div className="col-10 mx-auto">
-              <div className="row">
-                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-                  <h1>
-                    Our mission wih <strong className="brand-name">yoga</strong>{" "}
-                    is to provide easy and accessible exercise solutions to
-                  </h1>
-                  {/* sub tagline */}
-                  <h2 className="my-3">Find out how</h2>
-                  <div className="mt-3">
-                    <NavLink to="/service" className="btn-get-started">
-                      Check us Out
-                    </NavLink>
-                  </div>
-                </div>
-                {/* about image */}
-                <div className="col-lg-6 order-1 order-lg-2 header-img">
-                  <img
-                    src={yoga2}
-                    className="img-fluid animated"
-                    alt="home screen img"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      {/* Define props in the parent component that can be used to get data from the child component */}
+      <Common
+        name="Our mission is to make healthy lifestyle affordable and accessible to "
+        brandname="everyone."
+        tagline="All our services come with a 30 day money back guarantee*."
+        imgsrc={yoga2}
+        visit="/contact"
+        btnname="Contact Now"
+      />
     </>
   );
 };
