3d89441de50aee8b43002a55cd771ba3206fe2a8
maurya512 <patelmaurya0512@gmail.com>
2021-11-13 00:26:09 -0500

    changed the names from class to className

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import service1 from '../../images/yogaimg1.svg'
+import service1 from "../../images/yogaimg1.svg";
 const Card = () => {
   return (
     <div className="col-md-4 col-10 mx-auto">
-      <div class="card">
-        <img src={service1} class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
+      <div className="card">
+        <img src={service1} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
-          <NavLink to="#" class="btn btn-primary">
+          <NavLink to="#" className="btn btn-primary">
             Get Info
           </NavLink>
         </div>
