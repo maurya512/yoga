dfa69afcc78e510d4887903ff680dc5d688bb4a6
maurya512 <patelmaurya0512@gmail.com>
2021-11-13 00:19:39 -0500

    mapped the data from data file

---
+++ src/components/Card/CardData.jsx
@@ -0,0 +1,24 @@
+// the data file whose data will be rendered on the card
+import cdata1 from '../../images/yogaimg1.svg'
+
+const CData = [
+  {
+    imgsrc: cdata1,
+    title: "Power Yoga",
+  },
+  {
+    imgsrc: cdata1,
+    title: "Power Yoga",
+  },
+  {
+    imgsrc: cdata1,
+    title: "Power Yoga",
+  },
+  {
+    imgsrc: cdata1,
+    title: "Power Yoga",
+  },
+];
+
+// export the data file
+export default CData;
