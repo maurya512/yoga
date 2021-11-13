74cfe0259e6d4f92c55e6ad3cefc63060a43722b
maurya512 <patelmaurya0512@gmail.com>
2021-11-13 00:20:24 -0500

    modded the service component

--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,31 +1,24 @@
 import React from "react";
+import Card from "../../components/Card/Card";
+import CData from "../../components/Card/CardData";
 import service1 from "../../images/yogaimg1.svg";
 
 const Service = () => {
   return (
     <>
       <div className="my-5">
-        <h1 className="tab-center">Our Services</h1>
+        <h1 className="text-center">Our Services</h1>
       </div>
       <div className="container-fluid mb-5">
         <div className="row">
           <div className="col-10 mx-auto">
-            <div className="row">
-              <div className="col-md-4 col-10 mx-auto">
-                <div class="card">
-                  <img src={service1} class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                      Go somewhere
-                    </a>
-                  </div>
-                </div>
-              </div>
+            <div className="row gy-5">
+              {/* mapping the data from CData file */}
+              {/* mapping the data lets us access all the data and render it */}
+              {CData.map((val, ind) => {
+                return <Card imgsrc={val.imgsrc} title={val.title} />;
+              })}
+              <Card />
             </div>
           </div>
         </div>
