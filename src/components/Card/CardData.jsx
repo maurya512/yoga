7b8b6f15c084428eb1958381740c8836811f67ad
maurya512 <patelmaurya0512@gmail.com>
2021-11-16 23:08:06 -0500

    added new images and service description to the card

--- src/components/Card/CardData.jsx
+++ src/components/Card/CardData.jsx
@@ -1,22 +1,47 @@
 // the data file whose data will be rendered on the card
-import cdata1 from '../../images/yogaimg1.svg'
+import cdata1 from "../../images/aerialyoga.jpeg";
+import cdata2 from "../../images/yogaservice2.jpeg";
+import cdata3 from "../../images/yogaservice3.jpeg";
+import cdata4 from "../../images/restorative.jpeg";
+import cdata5 from "../../images/yogaservice5.jpeg";
+import cdata6 from "../../images/acroyoga.jpeg";
 
 const CData = [
   {
-    imgsrc: cdata1,
+    imgsrc: cdata2,
+    title: "Ashtanga",
+    description:
+      "Synchronizes breath, postures and Drishti (gaze point). Extremely beneficial for building strength, purifying the nervous system, and calming the mind.",
+  },
+  {
+    imgsrc: cdata3,
     title: "Power Yoga",
+    description:
+      "Power yoga emphasizes on a vinyasa (flowing) style of class, with flowing movements to get your heart rate up and build strength.",
+  },
+  {
+    imgsrc: cdata4,
+    title: "Restorative",
+    description:
+      "A rather newer form of yoga with therapeutic approach in mind. Use of props and other support system ensure the relaxation of nervous system.",
+  },
+  {
+    imgsrc: cdata5,
+    title: "Hathyoga",
+    description:
+      "Hatha is about creating balance in the autonomic nervous system by following strengthening poses with relaxing poses.",
+  },
+  {
+    imgsrc: cdata6,
+    title: "Acro Yoga",
+    description:
+      "Acro yoga uses yoga and gymnastic training to enable participants to achieve some incredible physical feats.",
   },
   {
     imgsrc: cdata1,
-    title: "Power Yoga",
-  },
-  {
-    imgsrc: cdata1,
-    title: "Power Yoga",
-  },
-  {
-    imgsrc: cdata1,
-    title: "Power Yoga",
+    title: "Aerial Yoga",
+    description:
+      "Aerial yoga involves using suspended hammocks to place the body in postures that may not otherwise be possible.",
   },
 ];
 
